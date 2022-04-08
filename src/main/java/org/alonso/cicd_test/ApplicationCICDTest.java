22e51f62953c3e33e27fb17bff4f090eeefb3ee1
David Alonso Tubio <davidtaragonha@gmail.com>
2022-04-08 19:41:01 +0200

    Initial commit with a dummy app to start to configure the cicd of GitHub
    Signed-off-by: David Alonso Tubio <davidtaragonha@gmail.com>

---
+++ src/main/java/org/alonso/cicd_test/ApplicationCICDTest.java
@@ -0,0 +1,11 @@
+package org.alonso.cicd_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationCICDTest {
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationCICDTest.class, args);
+    }
+}
